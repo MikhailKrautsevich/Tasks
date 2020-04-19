ac6e23787a913c71a479c590e0744dedc89d2c20
Misha <mihail4egevara@gmail.com>
2020-04-19 12:34:47 +0300

    try task 3

--- hometask_03_contacts/src/main/java/com/example/hometask_03_contacts/Activity_add.java
+++ hometask_03_contacts/src/main/java/com/example/hometask_03_contacts/Activity_add.java
@@ -2,15 +2,20 @@ package com.example.hometask_03_contacts;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 
 public class Activity_add extends Activity {
+
+    ImageButton btnBackFromAdd ;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_add);
+
+        btnBackFromAdd = findViewById(R.id.backFromAdd) ;
     }
 }
