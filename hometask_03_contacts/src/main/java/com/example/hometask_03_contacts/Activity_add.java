dbfe4a620805666fc9722aded16c878ea39bd3e7
Misha <mihail4egevara@gmail.com>
2020-04-23 01:18:33 +0300

    try task 3

--- hometask_03_contacts/src/main/java/com/example/hometask_03_contacts/Activity_add.java
+++ hometask_03_contacts/src/main/java/com/example/hometask_03_contacts/Activity_add.java
@@ -16,6 +16,7 @@ public class Activity_add extends Activity {
 
     private EditText nameAddText, phoneNumberAdd, emailAdd ;
     private RadioButton phoneNumberRadButton, emailRadButton ;
+    private RadioGroup radioGroup ;
     Intent answerIntent ;
 
     @Override
@@ -24,11 +25,11 @@ public class Activity_add extends Activity {
         setContentView(R.layout.activity_add);
         answerIntent = new Intent() ;
 
-        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.email_Or_phoneNumber);
-        phoneNumberRadButton = findViewById(R.id.phoneNumberRadButton) ;
-        emailRadButton = findViewById(R.id.emailRadButton) ;
-        radioGroup.addView(phoneNumberRadButton);
-        radioGroup.addView(emailRadButton);
+        radioGroup = (RadioGroup) findViewById(R.id.email_Or_phoneNumber);
+        phoneNumberRadButton = (RadioButton) findViewById(R.id.phoneNumberRadButton) ;
+        emailRadButton = (RadioButton) findViewById(R.id.emailRadButton) ;
+        radioGroup.clearCheck();
+
 
         nameAddText = findViewById(R.id.nameAddText) ;
         phoneNumberAdd = findViewById(R.id.phoneNumberAdd) ;
@@ -83,6 +84,30 @@ public class Activity_add extends Activity {
                 }
             }
         });
+
+ /*       radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case -1:
+                        Toast.makeText(getApplicationContext(), "не ок", Toast.LENGTH_SHORT).show(); ;
+                        break;
+                    case R.id.emailRadButton:
+                        phoneNumberAdd.setVisibility(View.INVISIBLE);
+                        emailAdd.setVisibility(View.VISIBLE);
+                        Toast.makeText(getApplicationContext(), "Email", Toast.LENGTH_SHORT).show(); ;
+                        break;
+                    case  R.id.phoneNumberRadButton:
+                        phoneNumberAdd.setVisibility(View.VISIBLE);
+                        emailAdd.setVisibility(View.INVISIBLE);
+                        Toast.makeText(getApplicationContext(), "Номер", Toast.LENGTH_SHORT).show(); ;
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        });  */
     }
 
     public void onRadioClick(View view) {
@@ -90,10 +115,12 @@ public class Activity_add extends Activity {
             case R.id.phoneNumberRadButton:
                 phoneNumberAdd.setVisibility(View.VISIBLE);
                 emailAdd.setVisibility(View.INVISIBLE);
+                emailRadButton.setChecked(false);
                 break;
             case R.id.emailRadButton:
                 phoneNumberAdd.setVisibility(View.INVISIBLE);
                 emailAdd.setVisibility(View.VISIBLE);
+                phoneNumberRadButton.setChecked(false);
                 break;
             default:
                 break;
